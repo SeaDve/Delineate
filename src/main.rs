9a8f78d75b4e209b6538349e54ef58588afdd4e0
Dave Patrick Caberto <davecruz48@gmail.com>
2023-12-14 09:33:21 +0800

    add more clippy lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![warn(
+    rust_2018_idioms,
+    clippy::items_after_statements,
+    clippy::needless_pass_by_value,
+    clippy::semicolon_if_nothing_returned,
+    clippy::match_wildcard_for_single_variants,
+    clippy::inefficient_to_string,
+    clippy::map_unwrap_or,
+    clippy::implicit_clone,
+    clippy::struct_excessive_bools,
+    clippy::unreadable_literal,
+    clippy::if_not_else,
+    clippy::doc_markdown,
+    clippy::unused_async,
+    clippy::default_trait_access,
+    clippy::unnecessary_wraps,
+    clippy::unused_self,
+    clippy::dbg_macro,
+    clippy::todo,
+    clippy::map_unwrap_or,
+    clippy::or_fun_call,
+    clippy::print_stdout
+)]
+
 mod about;
 mod application;
 mod config;
