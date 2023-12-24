0efc395184385a16676f519fb841cbeef3a83501
Dave Patrick Caberto <davecruz48@gmail.com>
2023-12-25 00:01:40 +0800

    add way to restore closed pages

--- src/session.rs
+++ src/session.rs
@@ -9,7 +9,10 @@ use gtk::{
 };
 use serde::{Deserialize, Serialize};
 
-use crate::{config::APP_ID, graph_view::LayoutEngine, window::Window, Application};
+use crate::{
+    config::APP_ID, document::Document, graph_view::LayoutEngine, page::Page, window::Window,
+    Application,
+};
 
 const DEFAULT_WINDOW_WIDTH: i32 = 1000;
 const DEFAULT_WINDOW_HEIGHT: i32 = 600;
@@ -20,21 +23,81 @@ static APP_DATA_DIR: Lazy<PathBuf> = Lazy::new(|| {
     path
 });
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Selection {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SelectionState {
     start_line: i32,
     start_line_offset: i32,
     end_line: i32,
     end_line_offset: i32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct PageState {
+impl SelectionState {
+    pub fn for_document(document: &Document) -> Option<Self> {
+        document
+            .selection_bounds()
+            .map(|(start, end)| SelectionState {
+                start_line: start.line(),
+                start_line_offset: start.line_offset(),
+                end_line: end.line(),
+                end_line_offset: end.line_offset(),
+            })
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PageState {
     paned_position: i32,
     is_active: bool,
-    uri: String,
-    selection: Option<Selection>,
+    uri: Option<String>,
+    selection: Option<SelectionState>,
     layout_engine: LayoutEngine,
+}
+
+impl PageState {
+    pub fn for_page(page: &Page) -> Self {
+        let document = page.document();
+
+        Self {
+            paned_position: page.paned_position(),
+            is_active: page.is_active(),
+            uri: document.file().map(|f| f.uri().into()),
+            selection: SelectionState::for_document(&document),
+            layout_engine: page.layout_engine(),
+        }
+    }
+
+    /// Restores the state into a page in the given window.
+    pub async fn restore(&self, window: &Window) -> Page {
+        let page = window.add_new_page();
+        page.set_paned_position(self.paned_position);
+        page.set_layout_engine(self.layout_engine);
+
+        if let Some(uri) = &self.uri {
+            let file = gio::File::for_uri(uri);
+            if let Err(err) = page.load_file(file).await {
+                tracing::error!(uri = self.uri, "Failed to load file for page: {:?}", err);
+            }
+        }
+
+        if let Some(SelectionState {
+            start_line,
+            start_line_offset,
+            end_line,
+            end_line_offset,
+        }) = self.selection
+        {
+            let document = page.document();
+            let start_iter = document
+                .iter_at_line_offset(start_line, start_line_offset)
+                .unwrap();
+            let end_iter = document
+                .iter_at_line_offset(end_line, end_line_offset)
+                .unwrap();
+            document.select_range(&start_iter, &end_iter);
+        }
+
+        page
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -44,6 +107,55 @@ struct WindowState {
     is_maximized: bool,
     is_active: bool,
     pages: Vec<PageState>,
+    closed_pages: Vec<PageState>,
+}
+
+impl WindowState {
+    fn for_window(window: &Window) -> Self {
+        let page_states = window
+            .pages()
+            .iter()
+            .map(PageState::for_page)
+            .collect::<Vec<_>>();
+
+        WindowState {
+            width: window.default_width(),
+            height: window.default_height(),
+            is_maximized: window.is_maximized(),
+            is_active: window.is_active(),
+            pages: page_states,
+            closed_pages: window.closed_pages(),
+        }
+    }
+
+    async fn restore(&self, session: &Session) -> Window {
+        let window = session.add_new_raw_window();
+        window.set_default_size(self.width, self.height);
+        window.set_maximized(self.is_maximized);
+        window.set_closed_pages(self.closed_pages.clone());
+
+        let mut active_page = None;
+        for page_state in &self.pages {
+            let page = page_state.restore(&window).await;
+
+            if page_state.is_active {
+                let prev_value = active_page.replace(page);
+                debug_assert!(prev_value.is_none());
+            }
+        }
+
+        if let Some(page) = active_page {
+            window.set_selected_page(&page);
+        }
+
+        window.present();
+
+        if self.pages.is_empty() {
+            window.add_new_page();
+        }
+
+        window
+    }
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -80,21 +192,7 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for Session {
-        fn dispose(&self) {
-            let obj = self.obj();
-
-            // FIXME This should be spawned asynchronously and called in last window closed
-            let ctx = glib::MainContext::default();
-            ctx.block_on(clone!(@weak obj => async move {
-                tracing::debug!("Saving session on dispose");
-
-                if let Err(err) = obj.save().await {
-                    tracing::error!("Failed to save session on dispose: {:?}", err);
-                }
-            }));
-        }
-    }
+    impl ObjectImpl for Session {}
 }
 
 glib::wrapper! {
@@ -154,6 +252,16 @@ impl Session {
         if matches!(imp.windows.borrow().as_slice(), [last_window] if last_window == window) {
             imp.default_window_width.set(window.default_width());
             imp.default_window_height.set(window.default_height());
+
+            // FIXME This causes EntryError on close_request
+            let ctx = glib::MainContext::default();
+            ctx.block_on(clone!(@weak self as obj => async move {
+                tracing::debug!("Saving session on dispose");
+
+                if let Err(err) = obj.save().await {
+                    tracing::error!("Failed to save session on dispose: {:?}", err);
+                }
+            }));
         }
 
         imp.windows.borrow_mut().retain(|w| w != window);
@@ -180,53 +288,7 @@ impl Session {
 
         let mut active_window = None;
         for window_state in &state.windows {
-            let window = self.add_new_raw_window();
-            window.set_default_size(window_state.width, window_state.height);
-            window.set_maximized(window_state.is_maximized);
-
-            let mut active_page = None;
-            for page_state in &window_state.pages {
-                let page = window.add_new_page();
-                page.set_paned_position(page_state.paned_position);
-                page.set_layout_engine(page_state.layout_engine);
-
-                let file = gio::File::for_uri(&page_state.uri);
-                if let Err(err) = page.load_file(file).await {
-                    tracing::error!(
-                        uri = page_state.uri,
-                        "Failed to load file for page: {:?}",
-                        err
-                    );
-                }
-
-                if let Some(Selection {
-                    start_line,
-                    start_line_offset,
-                    end_line,
-                    end_line_offset,
-                }) = page_state.selection
-                {
-                    let document = page.document();
-                    let start_iter = document
-                        .iter_at_line_offset(start_line, start_line_offset)
-                        .unwrap();
-                    let end_iter = document
-                        .iter_at_line_offset(end_line, end_line_offset)
-                        .unwrap();
-                    document.select_range(&start_iter, &end_iter);
-                }
-
-                if page_state.is_active {
-                    let prev_value = active_page.replace(page);
-                    debug_assert!(prev_value.is_none());
-                }
-            }
-
-            if let Some(page) = active_page {
-                window.set_selected_page(&page);
-            }
-
-            window.present();
+            let window = window_state.restore(self).await;
 
             if window_state.is_active {
                 let prev_value = active_window.replace(window);
@@ -243,7 +305,12 @@ impl Session {
             window.present();
         }
 
-        tracing::debug!(elapsed = ?now.elapsed(), ?state, "Session restored");
+        tracing::debug!(
+            elapsed = ?now.elapsed(),
+            path = %APP_DATA_DIR.display(),
+            ?state,
+            "Session restored"
+        );
 
         Ok(())
     }
@@ -253,54 +320,14 @@ impl Session {
 
         let now = Instant::now();
 
-        let mut windows_state = Vec::new();
-        for window in imp.windows.borrow().as_slice() {
-            let window = window.downcast_ref::<Window>().unwrap();
-
-            let pages = window.pages();
-
-            let mut pages_state = Vec::new();
-            for page in pages {
-                let document = page.document();
-
-                if document.is_safely_discardable() {
-                    continue;
-                }
-
-                let Some(file) = &document.file() else {
-                    continue;
-                };
-
-                let selection = document.selection_bounds().map(|(start, end)| Selection {
-                    start_line: start.line(),
-                    start_line_offset: start.line_offset(),
-                    end_line: end.line(),
-                    end_line_offset: end.line_offset(),
-                });
-                pages_state.push(PageState {
-                    paned_position: page.paned_position(),
-                    is_active: window.selected_page().as_ref() == Some(&page),
-                    uri: file.uri().into(),
-                    selection,
-                    layout_engine: page.layout_engine(),
-                });
-            }
-
-            if pages_state.is_empty() {
-                continue;
-            }
-
-            windows_state.push(WindowState {
-                width: window.default_width(),
-                height: window.default_height(),
-                is_maximized: window.is_maximized(),
-                is_active: window.is_active(),
-                pages: pages_state,
-            });
-        }
-
+        let window_states = imp
+            .windows
+            .borrow()
+            .iter()
+            .map(WindowState::for_window)
+            .collect::<Vec<_>>();
         let state = State {
-            windows: windows_state,
+            windows: window_states,
             window_width: imp.default_window_width.get(),
             window_height: imp.default_window_height.get(),
         };
@@ -318,7 +345,12 @@ impl Session {
             .await
             .map_err(|(_, err)| err)?;
 
-        tracing::debug!(elapsed = ?now.elapsed(), ?state, "Session saved");
+        tracing::debug!(
+            elapsed = ?now.elapsed(),
+            path = %APP_DATA_DIR.display(),
+            ?state,
+            "Session saved"
+        );
 
         Ok(())
     }
